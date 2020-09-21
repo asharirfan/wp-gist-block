aca378276cea7732e49e4cc88f8749667e477eb3
Ashar Irfan <mrasharirfan@gmail.com>
2020-09-22 02:19:33 +0500

    👌 IMPROVE: Gist block edit mode

--- src/block/gist-block/edit.js
+++ src/block/gist-block/edit.js
@@ -1,6 +1,16 @@
-import { RichText } from '@wordpress/block-editor';
+import {
+	Button,
+	Card,
+	CardBody,
+	TextControl,
+	Toolbar,
+	ToolbarButton,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import './editor.scss';
+import { useEffect } from '@wordpress/element';
+import { BlockControls } from '@wordpress/block-editor';
+
+// import './editor.scss';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -14,25 +24,62 @@ import './editor.scss';
  */
 export default function Edit( props ) {
 	const {
-		attributes: {
-			content,
-		},
+		attributes,
 		className,
 		setAttributes,
 	} = props;
 
-	// Update field content on change.
-	const onChangeContent = ( newContent ) => {
-		setAttributes( { content: newContent } );
-	};
+	useEffect( () => {
+		if ( '' === attributes.gistUrl ) {
+			setAttributes( { isEditing: true } );
+		}
+	}, [] );
 
 	return (
-		<RichText
-			tagName="p"
-			className={ className }
-			onChange={ onChangeContent }
-			value={ content }
-			placeholder={ __( 'AsharIrfan Gist Block Demo', 'gist-block' ) }
-		/>
+		<>
+			<BlockControls>
+				<Toolbar>
+					{ attributes.isEditing ? (
+						<ToolbarButton
+							icon="welcome-view-site"
+							label={ __( 'Switch to Preview', 'giphy-block' ) }
+							onClick={ () => setAttributes( { isEditing: false } ) }
+							disabled={ '' === attributes.gistUrl }
+						/>
+					) : (
+						<ToolbarButton
+							icon="edit"
+							label={ __( 'Switch to Edit', 'giphy-block' ) }
+							onClick={ () => setAttributes( { isEditing: true } ) }
+						/>
+					) }
+				</Toolbar>
+			</BlockControls>
+
+			<Card className={ className }>
+				<CardBody>
+					<h3>{ __( 'Gist Block', 'gist-block' ) }</h3>
+					<TextControl
+						label={ __( 'Gist URL', 'gist-block' ) }
+						placeholder={
+							__( 'Copy and paste the Gist URL here', 'gist-block' )
+						}
+						onChange={ ( gistUrl ) => setAttributes( { gistUrl } ) }
+						value={ attributes.gistUrl }
+					/>
+					<TextControl
+						label={ __( 'File name (Optional)', 'gist-block' ) }
+						placeholder={
+							__( 'Enter name of the file here.', 'gist-block' )
+						}
+						onChange={ ( fileName ) => setAttributes( fileName ) }
+						value={ attributes.fileName }
+					/>
+					<Button isSecondary={ true }>
+						{ __( 'Embed', 'gist-block' ) }
+					</Button>
+				</CardBody>
+			</Card>
+		</>
 	);
 }
