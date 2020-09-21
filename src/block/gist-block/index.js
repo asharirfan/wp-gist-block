7017b3bf4fa717aa37d029a8c3f6f47c1aaf292d
Ashar Irfan <mrasharirfan@gmail.com>
2020-09-22 02:19:01 +0500

    📦 NEW: Block icon and attributes

--- src/block/gist-block/index.js
+++ src/block/gist-block/index.js
@@ -1,8 +1,10 @@
 import edit from './edit';
-import save from './save';
+
+// import save from './save';
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
-import './style.scss';
+
+// import './style.scss';
 
 /**
  * Register block type definition.
@@ -12,20 +14,27 @@ import './style.scss';
  * @link https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
  */
 registerBlockType( 'ashar-irfan/gist-block', {
-	title: __( 'AsharIrfan Gist Block', 'gist-block' ),
-	icon: 'edit',
+	title: __( 'Gist Block', 'gist-block' ),
+	icon: 'media-code',
 	category: 'common',
 	keywords: [
 		__( 'AsharIrfan', 'gist-block' ),
 		__( 'GistBlock', 'gist-block' ),
 	],
 	attributes: {
-		content: {
-			type: 'array',
-			source: 'children',
-			selector: 'p',
+		gistUrl: {
+			type: 'string',
+			default: '',
+		},
+		fileName: {
+			type: 'string',
+			default: '',
+		},
+		isEditing: {
+			type: 'boolean',
+			default: false,
 		},
 	},
 	edit,
-	save,
+	save: () => null,
 } );
